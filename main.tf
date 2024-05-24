3cd08db80bf78620ce632689564d3cd0defb1ed3
Daniel Lloyd <daniel.lloyd@foghornconsulting.com>
2024-05-24 09:46:22 -0500

    modularize #1

--- main.tf
+++ main.tf
@@ -1,18 +1,106 @@
-locals {
-  transit_vnet_address_space = "10.0.0.0/16"
-  spoke_01_vnet_address_space = "10.1.0.0/16"
-  spoke_02_vnet_address_space = "10.2.0.0/16"
+data "azurerm_resource_group" "this" {
+  name = var.resource_group_name
+}
 
-  tags = {
-    use = "BU Terraform Testing"
-    createdBy = "Terraform"
-    owner = "Daniel Lloyd"
+resource "azurerm_virtual_network" "this" {
+  resource_group_name = var.resource_group_name
+  location            = data.azurerm_resource_group.this.location
+
+  name                = "Transit"  # TODO: rename to BU-Transit
+  
+  address_space       = [var.vnet_address_space]
+  tags                = var.tags
+}
+
+# Public
+resource "azurerm_subnet" "public" {
+  resource_group_name = var.resource_group_name
+  
+  name                 = "Public"
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = var.public_subnet_prefixes
+}
+
+resource "azurerm_route_table" "public" {
+  location            = data.azurerm_resource_group.this.location
+  resource_group_name = var.resource_group_name
+
+  name = "public"
+  tags = var.tags
+
+  route {
+    name           = "local"
+    address_prefix = var.vnet_address_space
+    next_hop_type  = "VnetLocal"
+  }
+
+  route {
+    name           = "internet"
+    address_prefix = "0.0.0.0/0"
+    next_hop_type  = "Internet"
   }
 }
 
-resource "azurerm_resource_group" "bu_tf_testing_transit_vnet" {
-  name     = "bu-tf-testing-transit-vnet"
-  location = "Central US"
+resource "azurerm_subnet_route_table_association" "public" {
+  route_table_id = azurerm_route_table.public.id
+  subnet_id      = azurerm_subnet.public.id
+}
 
-  tags = local.tags
+# Private
+resource "azurerm_subnet" "private" {
+  resource_group_name  = var.resource_group_name
+  
+  name                 = "Private"
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = var.private_subnet_prefixes
+}
+
+resource "azurerm_route_table" "private" {
+  location                      = data.azurerm_resource_group.this.location
+  resource_group_name           = var.resource_group_name
+
+  name = "private"
+  tags = var.tags
+
+  route {
+    name           = "local"
+    address_prefix = var.vnet_address_space
+    next_hop_type  = "VnetLocal"
+  }
+}
+
+resource "azurerm_subnet_route_table_association" "private" {
+  route_table_id = azurerm_route_table.private.id
+  subnet_id      = azurerm_subnet.private.id
+}
+
+# NAT Gateway
+resource "azurerm_public_ip" "nat_gw" {
+  location            = data.azurerm_resource_group.this.location
+  resource_group_name = var.resource_group_name
+
+  name                = "NAT-Gateway"
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  zones               = ["1"]
+}
+
+resource "azurerm_nat_gateway" "this" {
+  location                = data.azurerm_resource_group.this.location
+  resource_group_name     = var.resource_group_name
+
+  name                    = "NAT-Gateway"
+  sku_name                = "Standard"
+  idle_timeout_in_minutes = 10
+  zones                   = ["1"]
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "transit_private" {
+  nat_gateway_id       = azurerm_nat_gateway.this.id
+  public_ip_address_id = azurerm_public_ip.nat_gw.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "transit_private" {
+  subnet_id      = azurerm_subnet.private.id
+  nat_gateway_id = azurerm_nat_gateway.this.id
 }
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {}
+
+variable "vnet_address_space" {}
+
+variable "public_subnet_prefixes" {}
+
+variable "private_subnet_prefixes" {}
+
+variable "tags" {}
